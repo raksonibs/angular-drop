9e16698a4a69262ffe302e9796490e729257ebf9
Oskar Niburski <oskarniburski@gmail.com>
2015-06-02 19:10:26 +0300

    Nav and views

--- schematica/client/dbfield/dbfield.js
+++ schematica/client/dbfield/dbfield.js
@@ -1,8 +1,22 @@
+Template.dbfield.firstvar = function () {
+  return "First Var";
+}
 Template.dbfield.events({
-  'click .something': function (evt, tmpl) {
-    var name = tmpl.find('.name').value;
+  'click .icon-lock':function(evt,tmpl){
+    evt.stopPropagation();
+    evt.preventDefault();
+    Session.set('editing_field',this._id);
+  },
+  'keyup .efield':function(evt,tmpl){
+    evt.stopPropagation();
+    evt.preventDefault();
+    var fieldname = tmpl.find('.efield').value;
+    if(fieldname && evt.which == 13){
+      DBfields.update(this._id,{$set:{name:fieldname}});
+      Session.set('editing_field',null);
+    }
   }
 });
-Template.dbfield.firstvar = function() {
-  return "First Var"
-}
+Template.dbfield.editing_field = function(){
+  return Session.equals('editing_field',this._id);
+};
