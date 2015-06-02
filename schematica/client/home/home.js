f88c8abc9abaf82619242f7cafc2f01a61ee67c4
Oskar Niburski <oskarniburski@gmail.com>
2015-06-02 19:10:30 +0300

    Rest

--- schematica/client/home/home.js
+++ schematica/client/home/home.js
@@ -1,9 +1,15 @@
-Template.home.events({
-  'click .something': function (evt, tmpl) {
-    var name = tmpl.find('.name').value;
+Template.home.events ({
+  'dblclick .schema': function (evt, tmpl) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    if(evt.target.className === 'container-fluid schema'){
+      // instert atalbe at some thing
+      var id = Positions.insert({name:'New Table',left:(evt.pageX + 280) + 'px',top:(evt.pageY) + 'px'});
+      //  set session variable to be editable with that id. It is a global variable, table ebeing edited at this id. ?
+      Session.set('editing_table',id);
+    }
   }
 });
-// positions is a variable in home template, that returns all positions in collections.js
-Template.home.positions = function() {
+Template.home.positions = function () {
   return Positions.find();
 }
